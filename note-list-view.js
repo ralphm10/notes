e915709b048656d36130d6c03896e0584c47837a
Ralph Mallett <rmallett12@gmail.com>
2020-08-19 16:34:20 +0100

    Amend note list view to only return first 20 characters

--- note-list-view.js
+++ note-list-view.js
@@ -5,13 +5,13 @@
 
   exports.NoteListView = NoteListView;
 
-  NoteListView.prototype.getHTML = function () {
+  NoteListView.prototype.getHTML = function (limit = 20) {
 
     var notearray = this.list.getList();
     var textarray = [];
 
     for(let i = 0; i < notearray.length; i++) {
-      textarray.push(notearray[i].getText());
+      textarray.push(notearray[i].getText().slice(0,limit));
     }
 
     return '<ul><li><div>' + textarray.join('</div></li><li><div>') + '</div></li></ul>';
