122df69b7c37dce44104e8d85b489ae7eed9dbf6
Ralph Mallett <rmallett12@gmail.com>
2020-08-19 16:39:12 +0100

    Amend note list view test to only return first 20 characters of each note

--- note-list-view-tests.js
+++ note-list-view-tests.js
@@ -1,9 +1,9 @@
 function testNoteListViewTest() {
   var noteList = new NoteList();
-  noteList.createNew('some text')
-  noteList.createNew('more text')
+  noteList.createNew('The quick brown fox jumps over the lazy dog.')
+  noteList.createNew('This is a different note from the first one I passed in')
   var noteListView = new NoteListView(noteList);
-  assert.isTrue(noteListView.getHTML() === '<ul><li><div>some text</div></li><li><div>more text</div></li></ul>')
+  assert.isTrue(noteListView.getHTML() === '<ul><li><div>The quick brown fox </div></li><li><div>This is a different </div></li></ul>')
 };
 
 testNoteListViewTest();
